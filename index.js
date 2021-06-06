279fbfe6a2cb1e754b0818384d88c6a7f0205e00
ikuromori <83898553+ikuromori@users.noreply.github.com>
2021-06-06 09:25:34 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ $(function () {
     $('form').submit(function () {
         var creatinine = $('textarea[name="creatinine"]').val();
         var aged = $('textarea[name="aged"]').val();
-        var sex = $('radio[name="sex"]').val();        
-        var msg = `'クレアチニン'+${creatinine}\n'年齢'+${aged}\n'性別'+${sex}`;
+        var sex = $('input[name="sex"]:checked').val();        
+        var msg = `クレアチニン:${creatinine}\n年齢:${aged}\n性別:${sex}`;
         sendText(msg);
         return false;
     });
